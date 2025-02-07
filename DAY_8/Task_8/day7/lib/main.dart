078246ffeab1448d6f828f5d8953f2c750f35628
jonkolp <youssefhany2073@gmail.com>
2025-02-08 00:55:31 +0300

    Task 8

---
+++ DAY_8/Task_8/day7/lib/main.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: prefer_const_constructors
+
+
+import 'package:day_7/screens/categories.dart';
+import 'package:day_7/screens/home.dart';
+import 'package:day_7/screens/movie_screen.dart';
+import 'package:day_7/screens/product_list.dart';
+import 'package:day_7/screens/settings.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      // home: TabBarExample()
+      // BottomNavigationBarDemo(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        'settings': (context) => SettingPage(),
+        'categories': (context) => CategoriesPage(),
+        'products': (context) => ProductListPage(),
+        'movies': (context) => MovieListPage(),
+
+        // 'productDetails': (context) => ProductDetailsScreen(),
+       
+        
+      },
+    );
+  }
+}
